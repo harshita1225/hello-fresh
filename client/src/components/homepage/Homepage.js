df3a147961aecd4284af07b82f8ce8e780b9b09f
alinaincodeland <alinacuznetov@gmail.com>
2023-01-06 17:21:22 +0100

    added Footer file, edited WhatsInTheBox

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import ColumnSection from "./ColumnSection";
 import WhatsInTheBox from "./WhatsInTheBox";
+import Footer from "./Footer";
 
 const Homepage = () => {
   return (
@@ -11,6 +12,7 @@ const Homepage = () => {
       <Hero />
       <ColumnSection />
       <WhatsInTheBox />
+      <Footer />
     </div>
   );
 };
