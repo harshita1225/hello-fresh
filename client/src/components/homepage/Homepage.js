435030b3fb92a090af63a077779efff8788a5feb
alinaincodeland <alinacuznetov@gmail.com>
2023-01-06 12:03:34 +0100

    edited homepage

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
+import ColumnSection from "./ColumnSection";
+import WhatsInTheBox from "./WhatsInTheBox";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <ColumnSection />
+      <WhatsInTheBox />
     </div>
   );
 };
