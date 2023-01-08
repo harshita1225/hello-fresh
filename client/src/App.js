2c60847fd743cf2b5a194189a4a961e8b18dabd5
harshita <harshita1225@gmail.com>
2023-01-08 15:09:44 +0100

    edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./components/Register";
+import Address from "./components/plans/Address";
 import PaymentPage from "./components/plans/PaymentPage";
 import Homepage from "./components/homepage/Homepage";
 import PlansPage from "./components/plans/PlansPage";
+import Testtoggle from "./components/plans/Testtoggle";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<Register />} />
+          <Route path="/address" element={<Address />} />
           <Route path="/payment" element={<PaymentPage />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/plans" element={<PlansPage />} />
+          <Route path="/test" element={<Testtoggle />} />
         </Routes>
       </BrowserRouter>
     </div>
